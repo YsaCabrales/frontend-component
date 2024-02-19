9afc3f3d0b8daaab439edc75350c4971591f79c4
regelcabrales <regelcabrales@gmail.com>
2024-02-19 19:06:09 +0800

    Styled Login Page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -14,15 +14,25 @@ const Login = () => {
 	};
 
 	return (
-		<form className="login" onSubmit={handleSubmit}>
-			<h3>Login</h3>
-			<label>Username: </label>
-			<input type="text" onChange={(e) => setUsername(e.target.value)} value={username} />
-			<label>Password: </label>
-			<input type="password" onChange={(e) => setPassword(e.target.value)} value={password} />
-			<button disabled={isLoading}>Login</button>
-			{error && <div className="error">{error}</div>}
-		</form>
+		<div id="login-container">
+			<div id="box">
+				<form className="login" onSubmit={handleSubmit}>
+					<h3>Sign in to your Account</h3>
+					<label>Username: </label>
+					<input type="text" onChange={(e) => setUsername(e.target.value)} value={username} />
+
+					<br />
+
+					<label>Password: </label>
+					<input type="password" onChange={(e) => setPassword(e.target.value)} value={password} />
+
+					<br />
+
+					<button id="login-button" disabled={isLoading}>Login</button>
+					{error && <div className="error">{error}</div>}
+				</form>
+			</div>
+		</div>
 	);
 };
 
